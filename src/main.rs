547dc2e56e1e30e71757c90366939a13931570a1
synth-evol <ccroft323@gmail.com>
2023-11-20 13:19:35 -0700

    Do actual error handling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use rosu::Osu;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let api_key = args[1].clone();
-    
+
     let app = Router::new().route("/:user_name", get(
         move |path| global_score(path, api_key)));
     // run it with hyper on localhost:3000
@@ -19,13 +19,21 @@ async fn main() {
         .await
         .unwrap();
 }
-
 async fn global_score(Path(user_name): Path<String>, api_key:String) -> String {
     let osu = Osu::new(api_key);
 
-    let user_opt = osu.user(user_name).await;
+    let user_req = osu.user(user_name).await;
 
-    let user = user_opt.unwrap().unwrap();
+    let user_opt = match user_req {
+        Ok(x) => x,
+        Err(e) =>  return e.to_string(),
+    };
+
+    let user = match user_opt {
+        Some(x) => x,
+        None => return "No user with that username found".to_string(),
+    };
+
     let score = user.pp_rank.to_string();
     return score;
 }
