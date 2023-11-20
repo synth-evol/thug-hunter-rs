7cb731209035c3c864fff35a8042e4157243e28b
synth-evol <ccroft323@gmail.com>
2023-11-20 13:01:55 -0700

    Basic api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    extract::Path,
+    routing::get,
+    Router,
+};
+use std::env;
+use rosu::Osu;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let api_key = args[1].clone();
+    
+    let app = Router::new().route("/:user_name", get(
+        move |path| global_score(path, api_key)));
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn global_score(Path(user_name): Path<String>, api_key:String) -> String {
+    let osu = Osu::new(api_key);
+
+    let user_opt = osu.user(user_name).await;
+
+    let user = user_opt.unwrap().unwrap();
+    let score = user.pp_rank.to_string();
+    return score;
 }
