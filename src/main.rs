adfd151fc9ab1a8855edc510d9fe22515a801ca0
synth-evol <ccroft323@gmail.com>
2023-12-09 16:47:47 -0700

    Move client into appstate

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use axum::{
     Router,
     http::StatusCode,
 };
-use std::env;
+use std::{
+    env,
+    sync::Arc,
+};
 use rosu::Osu;
 
-#[derive(Clone)]
 struct AppState{
-    key: String,
+    client: Osu,
 }
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let api_key = args[1].clone();
-    let state = AppState{key: api_key};
+    let state = Arc::new(AppState{client: Osu::new(api_key)});
 
     let app = Router::new().route("/:user_name", get(global_score)).with_state(state);
     // run it with hyper on localhost:3000
@@ -25,10 +27,9 @@ async fn main() {
         .await
         .unwrap();
 }
-async fn global_score(Path(user_name): Path<String>, State(state): State<AppState>) -> Result<String, StatusCode> {
-    let osu = Osu::new(state.key);
+async fn global_score(Path(user_name): Path<String>, State(state): State<Arc<AppState>>) -> Result<String, StatusCode> {
 
-    let user_req = osu.user(user_name).await;
+    let user_req = state.client.user(user_name).await;
 
     let user_opt = match user_req {
         Ok(x) => x,
